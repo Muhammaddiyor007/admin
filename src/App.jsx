61888587241a67b99d4a941c897e2c042a44b6e8
Muhammaddiyor007 <odiljonovmuhammaddiyor63@gmail.com>
2025-03-19 11:42:00 +0500

    "Admin"

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css';
 import Login from './components/Login';
 import Home from './pages/Home';
